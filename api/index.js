b7ccd3207d33eeee9caaf15992f0cd738cfb58a4
WangAo <wao1999@qq.com>
2020-08-16 18:59:48 +0800

    修复输入非法uid的bug

--- api/index.js
+++ api/index.js
@@ -1,10 +1,16 @@
 const { fetchStats, renderSVG } = require("../src/stats-card");
+const { renderError } = require("../src/common.js")
 
 module.exports = async (req, res) => {
   const { id } = req.query;
 
   res.setHeader("Content-Type", "image/svg+xml");
   res.setHeader("Cache-Control", `public, max-age=7200`); // 7200秒（2小时） 缓存
+
+  const validId = /^[1-9]\d*$/;
+  if(!validId.test(id)) {
+    return res.send(renderError(`"${id}"不是一个合法uid`));
+  }
 
   const stats = await fetchStats(id);
   return res.send(renderSVG(stats));
--- src/stats-card.js
+++ src/stats-card.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+const { renderError } = require("./common.js")
 
 async function fetchStats(id) {
   const res = await axios.get(`https://www.luogu.com.cn/user/${id}?_contentOnly`)
@@ -44,17 +45,7 @@ const renderSVG = (stats, options) => {
   const { hideTitle } = options || {};
 
   if(hideInfo) {
-    return `
-  <svg xmlns="http://www.w3.org/2000/svg" width="500" height="60" viewBox="0 0 500 60" fill="none">
-    <style>.header { font: 600 18px 'Segoe UI', Ubuntu, Sans-Serif; fill: #e74c3c; animation: fadeInAnimation 0.8s ease-in-out forwards; }</style>
-    <rect data-testid="card-bg" x="0.5" y="0.5" rx="4.5" height="99%" stroke="#E4E2E2" width="99%" fill="#fffefe" stroke-opacity="1" />
-    <g data-testid="card-title" transform="translate(25, 35)">
-      <g transform="translate(0, 0)">
-        <text x="0" y="0" class="header" data-testid="header">用户开启了“完全隐私保护”，获取数据失败</text>
-      </g>
-    </g>
-  </svg>
-    `
+    return renderError("用户开启了“完全隐私保护”，获取数据失败");
   }
 
   let max = 0;
