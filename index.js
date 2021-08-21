de2fac48afe687b49588dc34fb87e51c82ef8992
WangAo <wao1999@qq.com>
2021-08-21 22:26:02 +0800

    fix bug

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = {
-    ...require("src/guzhi-card"),
-    ...require("src/stats-card"),
-    ...require("src/common.js"),
+    ...require("./src/guzhi-card"),
+    ...require("./src/stats-card"),
+    ...require("./src/common.js"),
 }
